0731597f1184bbea6715f5d4ed2e2c1ba9160f73
EnzoAsplanatti <117104663+EnzoAA004@users.noreply.github.com>
2026-06-14 22:01:30 -0300

    Add reports route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { AboutPage } from "./pages/AboutPage";
 import { DashboardPage } from "./pages/DashboardPage";
 import { DatasetPage } from "./pages/DatasetPage";
 import { MeasurementsPage } from "./pages/MeasurementsPage";
+import { ReportsPage } from "./pages/ReportsPage";
 import { SimulationsPage } from "./pages/SimulationsPage";
 import { TheoryPage } from "./pages/TheoryPage";
 
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/measurements" element={<MeasurementsPage />} />
         <Route path="/dataset" element={<DatasetPage />} />
         <Route path="/simulations" element={<SimulationsPage />} />
+        <Route path="/reports" element={<ReportsPage />} />
         <Route path="/theory" element={<TheoryPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
